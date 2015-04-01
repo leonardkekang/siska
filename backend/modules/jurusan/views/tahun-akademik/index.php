4e859d3bb5d7f2d3b56b2923481bf8b3b4912865
Leonard Luangga Kekang <leonardkekang@gmail.com>
2015-04-01 15:33:36 +0800

    layout

--- backend/modules/jurusan/views/tahun-akademik/index.php
+++ backend/modules/jurusan/views/tahun-akademik/index.php
@@ -6,14 +6,12 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Tahun Akademiks';
+$this->title = 'Tahun Akademik';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tahun-akademik-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
+    <p class="pull-right">
         <?= Html::a('Create Tahun Akademik', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
@@ -22,12 +20,11 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'kode_tahun_akademik',
             'tahun_akademik',
             'semester',
             'status',
             'tanggal_mulai',
-            // 'tanggal_berakhir',
+            'tanggal_berakhir',
             // 'created_by',
             // 'updated_by',
             // 'created_at',
--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -1,36 +1,36 @@
 <aside class="main-sidebar">
 
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
 
-          <!-- Sidebar user panel (optional) -->
-          <div class="user-panel">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
             <div class="pull-left image">
-              <img src="http://localhost/app-kampus/siska-yii/dashboard/adminlte/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="http://localhost/app-kampus/siska-yii/dashboard/adminlte/img/user2-160x160.jpg" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
-              <p>Zaenal Abidin</p>
-              <!-- Status -->
-              <a href="#"><i class="fa fa-circle text-success"></i> Administrator</a>
+                <p>Zaenal Abidin</p>
+                <!-- Status -->
+                <a href="#"><i class="fa fa-circle text-success"></i> Administrator</a>
             </div>
-          </div>
+        </div>
 
-          <!-- search form (Optional) -->
-          <form action="#" method="get" class="sidebar-form">
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
+                <input type="text" name="q" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
+                </span>
             </div>
-          </form>
-          <!-- /.search form -->
+        </form>
+        <!-- /.search form -->
 
-          <!-- Sidebar Menu -->
-          <ul class="sidebar-menu">
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
             <!-- Optionally, you can add icons to the links -->
             <li><a href="http://localhost/app-kampus/siska-yii/dashboard/jurusan/tahun-akademik"><span>Tahun Akademik</span></a></li>
-          </ul><!-- /.sidebar-menu -->
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
